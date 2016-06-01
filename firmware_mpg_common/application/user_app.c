974c3bbdc57e3c3043392af6e6e76f5e2a05959c
yangtianqi24 <921269310@qq.com>
2016-06-01 10:13:58 +0800

    ledchange
    my changes are  complicated and I can't describe it very well.It is a
    combination of two lights.And it will gradually shine and out from two
    sides.In addition,the LCD also blink all the time.

--- firmware_mpg_common/application/user_app.c
+++ firmware_mpg_common/application/user_app.c
@@ -87,7 +87,25 @@ Promises:
   - 
 */
 void UserAppInitialize(void)
-{
+{/*turn on led using the on function*/
+ /* All discrete LEDs to off */
+  /* Start with red LED on 100%, green and blue off */
+  LedPWM(LCD_RED, LED_PWM_100);
+  LedPWM(LCD_GREEN, LED_PWM_0);
+  LedPWM(LCD_BLUE, LED_PWM_0);
+  LedOff(WHITE);
+  LedOff(PURPLE);
+  LedOff(BLUE);
+  LedOff(CYAN);
+  LedOff(GREEN);
+  LedOff(YELLOW);
+  LedOff(ORANGE);
+  LedOff(RED);
+  
+  /* Backlight to white */  
+  LedOn(LCD_RED);
+  LedOn(LCD_GREEN);
+  LedOn(LCD_BLUE);
   
   /* If good initialization, set state to Idle */
   if( 1 )
@@ -137,8 +155,187 @@ State Machine Function Definitions
 /* Wait for a message to be queued */
 static void UserAppSM_Idle(void)
 {
+  #ifdef MPG2
+  static LedNumberType aeCurrentLed[]  = {GREEN0, RED0, BLUE0, GREEN0, RED0, BLUE0};
+  static LedNumberType aeCurrentLed1[] = {GREEN1, RED1, BLUE1, GREEN1, RED1, BLUE1};
+  static LedNumberType aeCurrentLed2[] = {GREEN2, RED2, BLUE2, GREEN2, RED2, BLUE2};
+  static LedNumberType aeCurrentLed3[] = {GREEN3, RED3, BLUE3, GREEN3, RED3, BLUE3};
+#endif /* MPG2 */
+
+#ifdef MPG1
+  static LedNumberType aeCurrentLed[]  = {LCD_GREEN, LCD_RED, LCD_BLUE, LCD_GREEN, LCD_RED, LCD_BLUE};
+  
+#endif /* MPG 1 */
+  static bool abLedRateIncreasing[]   = {TRUE,      FALSE ,TRUE,     FALSE,     TRUE,    FALSE};
+  static u8 u8CurrentLedIndex  = 0;
+  static u8 u8LedCurrentLevel  = 0;
+  static u8 u8DutyCycleCounter = 0;
+  static u16 u16Counter = COLOR_CYCLE_TIME;
+  u16Counter--;
+  /* Check for update color every COLOR_CYCLE_TIME ms */  
+  if(u16Counter == 0)
+  {
+    u16Counter = COLOR_CYCLE_TIME;
+
+    /* Update the current level based on which way it's headed */
+
+    /* Update the value to the current LED */   
+    LedPWM( (LedNumberType)aeCurrentLed[u8CurrentLedIndex], (LedRateType)u8LedCurrentLevel);
+  
+   
+  /* Update the current level based on which way it's headed */
+    if( abLedRateIncreasing[u8CurrentLedIndex] )
+    {
+      u8LedCurrentLevel++;
+    }
+    else
+    {
+      u8LedCurrentLevel--;
+    }
+
+    /* Change direction once we're at the end */
+    u8DutyCycleCounter++;
+    if(u8DutyCycleCounter == 20)
+    {
+      u8DutyCycleCounter = 0;
+      
+      /* Watch for the indexing variable to reset */
+      u8CurrentLedIndex++;
+      if(u8CurrentLedIndex == 6)
+      {
+        u8CurrentLedIndex = 0;
+      }
+      
+      /* Set the current level based on what direction we're now going */
+      u8LedCurrentLevel = 20;
+      if(abLedRateIncreasing[u8CurrentLedIndex])
+      {
+         u8LedCurrentLevel = 0;
+      }
     
-} /* end UserAppSM_Idle() */
+    
+  }
+}
+ static u16 u16BlinkCount = 0;
+   static u8 u8Counter = 0;
+
+  u16BlinkCount++;
+  if(u16BlinkCount == 20)
+  {
+    u16BlinkCount = 0;
+    
+    /*Update the counter and roll at 16 */
+    u8Counter++;
+    if(u8Counter == 8)
+    {
+      LedOn(RED);
+    }
+    if(u8Counter == 16)
+    {
+      LedOn(ORANGE);;
+    }
+    if(u8Counter == 24)
+    {
+      LedOn(YELLOW);LedOff(RED);
+    }
+    if(u8Counter == 32)
+    {
+      LedOn(GREEN);LedOff(ORANGE);
+    }
+    if(u8Counter == 38)
+    {
+      LedOn(CYAN);LedOff(YELLOW);
+    }
+    if(u8Counter == 46)
+    {
+      LedOn(BLUE);LedOff(GREEN);
+    }
+    if(u8Counter == 54)
+    {
+      LedOn(PURPLE);LedOff(CYAN);
+    }
+    if(u8Counter == 62)
+    {
+      LedOn(WHITE);LedOff(BLUE);
+    }
+    if(u8Counter == 70)
+    {
+      LedOn(RED);
+    }
+    if(u8Counter == 78)
+    {
+      LedOn(ORANGE);;
+    }
+    if(u8Counter == 86)
+    {
+      LedOn(YELLOW);LedOff(RED);
+    }
+    if(u8Counter == 94)
+    {
+      LedOn(GREEN);LedOff(ORANGE);
+    }
+    if(u8Counter == 102)
+    {
+      LedOn(CYAN);LedOff(YELLOW);
+    }
+    if(u8Counter == 110)
+    {
+      LedOn(BLUE);LedOff(GREEN);
+    }if(u8Counter == 118)
+    {
+      LedOn(RED);
+    }
+    if(u8Counter == 126)
+    {
+      LedOn(ORANGE);
+    }
+    if(u8Counter == 132)
+    {
+      LedOn(YELLOW);LedOff(RED);
+    }
+    if(u8Counter == 140)
+    {
+      LedOn(GREEN);LedOff(ORANGE);
+    }
+    if(u8Counter == 148)
+    {
+      LedOn(RED);
+    }
+    if(u8Counter == 156)
+    {
+      LedOn(ORANGE);
+    }
+    if(u8Counter == 164)
+    {
+     
+      LedOff(RED);
+      LedOff(WHITE);
+    }
+    if(u8Counter == 172)
+    {
+     
+      LedOff(ORANGE);
+      LedOff(PURPLE);
+    }
+    if(u8Counter == 180)
+    {
+     
+      LedOff(YELLOW);
+      LedOff(BLUE);
+    }
+    if(u8Counter == 188)
+    {
+     
+      LedOff(GREEN);
+      LedOff(CYAN);
+    }
+    if(u8Counter == 188)u8Counter =0;
+    
+    
+  
+  
+  }
+} /* end if(u16BlinkCount == 500) */
      
 
 /*-------------------------------------------------------------------------------------------------------------------*/
